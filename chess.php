9f8917afc8cf582dca5938eb1df18c2a8bdac682
Vanya <56477758+Vanya1808@users.noreply.github.com>
2021-01-25 19:33:07 +0300

    1

--- chess.php
+++ chess.php
@@ -1,3 +1,16 @@
-rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR
+<?php
 
+session_start();
+if (strlen($_SESSION['map']) != 64)
+$_SESSION['map'] = 'rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR'
 
+if(isset ($_GET['getFigures']))
+   echo $_SESSION['map'];
+if (isset($_GET['moveFigure'])) {
+    $frCoord = $_GET['frCoord'];
+    $toCoord = $_GET['toCoord']
+    $figure = $_SESSION['map'][$frCoord];
+    $_SESSION['map'][frCoord] = '1';
+    $_SESSION['map'][toCoord] = '$figure';
+    echo $_SESSION['map']
+}
